14b98420e7adf153a937f2aa5be3896880dd1078
jakin <jakindaodhiambo@gmail.com>
2020-04-11 14:12:02 +0300

    first commit

---
+++ src/entry.py
@@ -0,0 +1,61 @@
+from simplexml import dumps
+from flask import Flask, request, jsonify, g
+#from flask_restful import Resource, Api, reqparse
+from .estimator import estimator
+import time
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.before_request
+def before_req():
+    g.start = time.time() * 1000
+
+
+@app.after_request
+def after_req(response):
+    f = open('logs.txt', 'a+')
+    req_method = request.method
+    req_path = request.path
+    res_time = round(time.time() * 1000 - g.start)
+    res_status_code = response.status_code
+
+    f.write("{} \t\t {} \t\t {} \t\t {} ms \n".format(req_method, req_path, res_status_code, res_time))
+    f.close()
+    return response
+
+
+@app.route('/')
+def home():
+    return "Building for SDG Andela with Facebook"
+
+
+@app.route('/api/v1/on-covid-19', methods=['POST'])
+def get_estimation_default():
+    req_data = request.get_json()
+    res = estimator(req_data)
+    return jsonify(res)
+
+
+@app.route('/api/v1/on-covid-19/json', methods=['POST'])
+def get_estimation_json():
+    return get_estimation_default()
+
+
+@app.route('/api/v1/on-covid-19/xml', methods=['POST'])
+def get_estimation_xml():
+    req_data = request.get_json()
+    res = dumps({'response': estimator(req_data)})
+    return res
+
+
+@app.route('/api/v1/on-covid-19/logs', methods=['GET'])
+def get_logs():
+    f = open('logs.txt', 'r')
+    contents = f.read()
+    f.close()
+    return contents
+
+
+app.run()
